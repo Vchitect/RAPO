987bbe7f339f010a112d82ecd421239242d4c3e8
WhynotHAHA <1460071957@qq.com>
2025-10-27 01:07:18 +0800

    Update refactoring.sh

--- examples/Stage1_RAPO/refactoring.sh
+++ examples/Stage1_RAPO/refactoring.sh
@@ -1,4 +1,4 @@
 python refactoring.py \
---mode_path "./ckpt/llama3_1_instruct_lora_rewrite" \
+--mode_path "../../ckpt/llama3_1_instruct_lora_rewrite" \
 --input_word_augmentation "./output/refactor/merging_reuslts.txt" \
 --output_refactoring "./output/refactor/refactoring_reuslts.txt" \
